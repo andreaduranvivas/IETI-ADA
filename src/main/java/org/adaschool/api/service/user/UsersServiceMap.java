d7e34196a5699ccbfc4f29ac55a9fcebf9f3075e
Andrea <andrea.duran@mail.escuelaing.edu.co>
2024-02-12 16:22:59 -0500

    swagger

---
+++ src/main/java/org/adaschool/api/service/user/UsersServiceMap.java
@@ -0,0 +1,38 @@
+package org.adaschool.api.service.user;
+
+import org.adaschool.api.repository.user.User;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public class UsersServiceMap implements UsersService {
+    private Map<String, User> users = new HashMap<>();
+
+    @Override
+    public User save(User user) {
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    @Override
+    public Optional<User> findById(String id) {
+        return Optional.ofNullable(users.get(id));
+    }
+
+    @Override
+    public List<User> all() {
+        return new ArrayList<>(users.values());
+    }
+
+    @Override
+    public void deleteById(String id) {
+        users.remove(id);
+    }
+
+    @Override
+    public User update(User user, String userId) {
+        users.put(userId, user);
+        return user;
+    }
+}
